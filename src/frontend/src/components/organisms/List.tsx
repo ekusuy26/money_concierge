e8594a4444bef712347f424476261055cf567b44
ekusuy26 <y.yuu6221@gmail.com>
2023-08-14 11:21:03 +0900

    set type

--- src/frontend/src/components/organisms/List.tsx
+++ src/frontend/src/components/organisms/List.tsx
@@ -1,19 +1,16 @@
 import useSWR from "swr";
 import { fetcher } from "@/Fetcher";
-import { useState } from "react";
-import { ListProps } from "@/interfaces/interface";
+import { Finance, ListProps } from "@/interfaces/interface";
 import ListContent from "@/components/molecules/ListContent";
 import Load from "@/components/molecules/Load";
 
 export default function List({ callback }: ListProps) {
-  const { data, error, isLoading } = useSWR(
+  const { data, error, isLoading } = useSWR<Finance[]>(
     `${process.env.NEXT_PUBLIC_API_URL}/finance`,
     fetcher
   );
 
-  const [item, setItem] = useState(0);
-
-  const clickedContent = (finance) => {
+  const clickedContent = (finance: Finance) => {
     callback(finance);
   };
 
@@ -21,14 +18,15 @@ export default function List({ callback }: ListProps) {
   if (isLoading) return <Load status="now" />;
   return (
     <>
-      {data.map((finance, i) => (
-        <ListContent
-          key={finance.id}
-          date={data[i - 1]?.formatDate !== finance.formatDate}
-          finance={finance}
-          callback={() => clickedContent(finance)}
-        />
-      ))}
+      {data &&
+        data.map((finance, i) => (
+          <ListContent
+            key={finance.id}
+            date={data[i - 1]?.formatDate !== finance.formatDate}
+            finance={finance}
+            callback={() => clickedContent(finance)}
+          />
+        ))}
     </>
   );
 }
